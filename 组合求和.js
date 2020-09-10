6374f337943df559dff20b6d84d62454a3dd9ad0
lifei-2019 <940915122@qq.com>
2020-09-10 18:50:00 +0800

    组合求和（方法二）

--- 组合求和.js
+++ 组合求和.js
@@ -1,4 +1,7 @@
 // 回溯，dfs
+// 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+// candidates 中的数字可以无限制重复被选取。
 combinationSum = function(candidates, target) {
   const ans = [];
   const dfs = (target, combine, idx) => {
@@ -19,3 +22,22 @@ combinationSum = function(candidates, target) {
   dfs(target, [], 0);
   return ans;
 }
+
+// 方法二
+const combinationSum = (candidates, target) => {
+    const res = [];
+    const dfs = (start, temp, sum) => {
+      if (sum >= target) {
+        if (sum == target) {
+          res.push(temp.slice()); // temp的拷贝
+        }
+        return;     // 结束当前递归
+      }
+      for (let i = start; i < candidates.length; i++) { // 枚举出选择，从start开始
+        temp.push(candidates[i]);           // 加入“部分解”
+        dfs(i, temp, sum + candidates[i]);  // 往下继续选择，同时sum累加上当前数字
+        temp.pop();                         // 撤销选择
+      }
+    };
+    dfs(0, [], 0);
+    return res;}
