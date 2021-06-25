b68fac50433a4f6c35009d066cf5ada4e26eb627
erekli <erekli@tencent.com>
2021-06-25 19:13:36 +0800

    map

--- 无重复字符的最长子串.js
+++ 无重复字符的最长子串.js
@@ -14,3 +14,25 @@ var lengthOfLongestSubstring = function(s) {
   }
   return max
 }
+
+
+
+
+const longset = (s)=>{
+  let ans =0
+  let i=0
+  let j=0
+  const map=new Map()
+
+  while(j<s.length){
+    const char = s[j]
+    const pos = map.get(char)
+    if(pos != undefined&&pos>=i){
+      i=pos+1
+    }
+    map.set(char,j)
+    ans = Math.max(j-i+1,ans)
+    j++
+  }
+  return ans
+}
