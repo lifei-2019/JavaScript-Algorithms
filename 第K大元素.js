70e94f3e2f4b22a4af7f238c0fdb24d9f7c8166b
erekli <erekli@tencent.com>
2021-07-01 16:33:06 +0800

    快排和优化、堆排序可以再看看

---
+++ 第K大元素.js
@@ -0,0 +1,65 @@
+const findKth1 = (nums,k)=>{
+    nums.sort((a,b)=>b-a)
+    return nums[k-1]
+}
+
+// 快排和优化
+const findKth2 = (nums,k)=>{
+    let quickSort = arr =>{
+        quick(arr, 0 ,arr.length-1)
+    }
+
+    let quick =(arr, left, right)=>{
+        let index 
+        // if(left<right){
+        //     // 划分数组
+        //     index = partition(arr,left,right)
+        //     if(left<index-1){
+        //         quick(arr,left,index-1)
+        //     }
+        //     if(right>index){
+        //         quick(arr,index,right)
+        //     }
+        // }
+
+        // 优化
+            if(left<right){
+                if(k === index) {
+                    return arr[index]
+                } else if(k < index) {
+                    // Top k 在左边
+                    return quick(arr, left, index-1, k)
+                } else {
+                    // Top k 在右边
+                    return quick(arr, index+1, right, k)
+                }
+            }
+        return arr[left]
+        
+      }
+      
+
+    // 一次交换
+    let partition = (arr,left,right) =>{
+        let datum = arr[Math.floor(Math.random()*(right-left+1))+left]
+        let i=left,j=right
+        while(i<=j){
+            while(arr[i]<datum){
+                i++
+            }
+            while(arr[j]>datum){
+                j--
+            }
+            if(i<=j){
+                [arr[i],arr[j]]=[arr[j],arr[i]]
+                i++
+                j--
+            }
+        }
+        return i
+    }
+    quickSort(nums)
+    console.log(nums)
+    return nums[arr.length - k]
+}
+
